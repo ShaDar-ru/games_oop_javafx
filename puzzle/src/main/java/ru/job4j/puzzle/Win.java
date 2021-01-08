843425a14cfec7b1a5820d062ea8e529a3ee90db
ShaDar <alexterentev90@gmail.com>
2021-01-08 15:15:00 +0300

    2. Сокобан. Тестовое задание. [#429286]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,31 +3,38 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
-        int sumRow=0;
-        int sumCol=0;
         for (int i = 0; i < board.length; i++) {
             if (board[i][i] == 1) {
-                for(int j =0;j<board.length;j++){
-                    sumCol=sumCol+board[i][j];
-                    sumRow=sumRow+board[j][i];
+                if (monoVertical(board, i) || monoHorizontal(board, i)) {
+                    rsl = true;
+                    break;
                 }
-                if(sumCol==5||sumRow==5){
-                    rsl=true;
-                }
-                sumCol=sumRow=0;
             }
         }
         return rsl;
     }
 
-    public static boolean checkLine(int[] line) {
-        boolean rsl = true;
-        for (int a : line) {
-            if (a != 1) {
-                rsl = false;
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                result = false;
                 break;
             }
         }
-        return rsl;
+        return result;
+    }
+
+    public static boolean monoVertical(int[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }
+
+
